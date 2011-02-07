425977034f06f2084fb545fd31959f5128474125
Paul Campbell <paul@rslw.com>
2011-02-07 21:01:26 +0000

    capture those blocks

--- app/helpers/pugita_engine_helper.rb
+++ app/helpers/pugita_engine_helper.rb
@@ -1,13 +1,13 @@
 module PugitaEngineHelper
-  def pugita_header
-    additional_header = block_given? ? yield : ''
+  def pugita_header(&block)
+    additional_header = block_given? ? capture(&block) : ''
     render :partial => 'pugita/header', :locals => {
       :additional_header => additional_header
     }
   end
   
-  def pugita_footer
-    additional_footer = block_given? ? yield : ''
+  def pugita_footer(&block)
+    additional_footer = block_given? ? capture(&block) : ''
     render :partial => 'pugita/footer', :locals => {
       :additional_footer => additional_footer
     }
