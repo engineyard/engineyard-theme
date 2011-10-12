4964e504b4d456754fd3452647ab293590d865e1
Gerlando Piro <gerlando@gmail.com>
2011-10-12 14:44:45 -0700

    fixed asset config

--- example/config/application.rb
+++ example/config/application.rb
@@ -2,9 +2,12 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# If you have a Gemfile, require the gems listed there, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env) if defined?(Bundler)
+if defined?(Bundler)
+  # If you precompile assets before deploying to production, use this line
+  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  # If you want your assets lazily compiled in production, use this line
+  # Bundler.require(:default, :assets, Rails.env)
+end
 
 module Example
   class Application < Rails::Application
@@ -45,5 +48,6 @@ module Example
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.assets.precompile = ['*.css','*.js','*.gif','*.jpg','*.png'] 
   end
 end
